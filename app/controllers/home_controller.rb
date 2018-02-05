c85ea7a5031f1a525d5bc6f23089653ef2202409
Eliav Lavi <eliavlavi@gmail.com>
2018-02-05 22:40:59 +0200

    added first sketch of chat

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def welcome
   end
+
+  def send_message
+    ActionCable.server.broadcast 'chat_channel', message: params[:message]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: 'home#welcome'
+
+  post '/send_message' => 'home#send_message'
 end
