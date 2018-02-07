c47e14a718b7a0c7050351e0ce5328cf265b973b
Eliav Lavi <eliavlavi@gmail.com>
2018-02-07 11:45:33 +0200

    added required: true for text fields and manage nickname in params and not in session for less stickiness

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,21 +1,18 @@
 class HomeController < ApplicationController
   def welcome
-    render :chat if session[:nickname].present?
   end
 
   def login
-    session[:nickname] = params[:nickname]
     render :chat
   end
 
   def send_message
-    ActionCable.server.broadcast 'chat_channel', nickname: session[:nickname], message: params[:message]
+    ActionCable.server.broadcast 'chat_channel', nickname: params[:nickname], message: params[:message]
 
     respond_to :js
   end
 
   def quit_chat
-    reset_session
     render :welcome
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get   'login'         => 'home#welcome'
   post  'login'         => 'home#login'
+  get   'chat'          => 'home#chat'
+
   post  'send_message'  => 'home#send_message'
   get   'quit_chat'     => 'home#quit_chat'
 end
